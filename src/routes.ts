768a2da89ef028723f4eb263138a7566e4b97ec1
Ian Schwartz <ischwartz@cargurus.com>
2025-04-03 10:13:16 -0400

    a little refactoring

---
+++ src/routes.ts
@@ -0,0 +1,68 @@
+import { HistoryManager } from './history.js';
+import { ZSHRC } from './zshrc.js';
+import { promptForAlias, promptForFunction, promptForEntryToDelete } from './prompts.js';
+import { generateShellCommand } from './function-executor.js';
+
+export interface Options {
+  input?: string;
+  alias?: string | boolean;
+  delete?: string | boolean;
+  lambda?: string | boolean;
+  noYarn?: boolean;
+}
+
+export const aliasPath = async (options: Options) => {
+  const lastCommand = HistoryManager.lastCommand;
+  const alias = typeof options.alias === 'string' ? options.alias : await promptForAlias();
+  if (!alias) {
+    console.log('No alias provided. Exiting...');
+    process.exit(0);
+  }
+  ZSHRC.updateZshrc(alias, lastCommand);
+}
+
+export const deletePath = async (options: Options) => {
+  if (typeof options.delete === 'string') {
+    // Delete specific entry
+    ZSHRC.deleteFromZshrc(options.delete);
+  } else {
+    // Interactive deletion
+    const entries = ZSHRC.listEntries();
+    const nameToDelete = await promptForEntryToDelete(entries);
+    if (nameToDelete) {
+      ZSHRC.deleteFromZshrc(nameToDelete);
+    }
+  }
+}
+
+export const lambdaPath = async (options: Options) => {
+  let func: string;
+      
+  if (typeof options.lambda === 'string') {
+    // Function was provided as an argument
+    func = options.lambda;
+  } else if (process.stdin.isTTY) {
+    // Interactive mode
+    func = await promptForFunction();
+  } else {
+    // Non-interactive mode but no function provided
+    console.error('Error: Function argument required when piping output');
+    process.exit(1);
+  }
+
+  if (!func) {
+    console.log('No function provided. Exiting...');
+    process.exit(0);
+  }
+  
+  // Get all arguments after the -l flag
+  const lambdaArgs = process.argv.slice(process.argv.indexOf('-l') + 1);
+  const command = generateShellCommand(func, lambdaArgs);
+  process.stdout.write(command + '\n');
+  
+  // Prompt for an alias
+  const alias = await promptForAlias();
+  if (alias) {
+    ZSHRC.updateZshrc(alias, command);
+  }
+} 
