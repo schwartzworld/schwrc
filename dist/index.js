d38fcea6f25d6c4c86a0d16b6b860c1f0d5d3f2c
Ian Schwartz <ischwartz@cargurus.com>
2025-04-03 10:13:58 -0400

    more refactoring

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
-import { HistoryManager } from './history.js';
-import { ZSHRC } from './zshrc.js';
-import { promptForAlias, promptForFunction, promptForEntryToDelete } from './prompts.js';
-import { generateShellCommand } from './function-executor.js';
+import { aliasPath, deletePath, lambdaPath } from './routes.js';
 const program = new Command();
 program
     .name('schwrc')
@@ -16,56 +13,13 @@ program
     .argument('[args...]', 'arguments to pass to the function')
     .action(async (args, options) => {
     if (options.alias !== undefined) {
-        const lastCommand = HistoryManager.lastCommand;
-        const alias = typeof options.alias === 'string' ? options.alias : await promptForAlias();
-        if (!alias) {
-            console.log('No alias provided. Exiting...');
-            process.exit(0);
-        }
-        ZSHRC.updateZshrc(alias, lastCommand);
+        return aliasPath(options);
     }
     else if (options.delete !== undefined) {
-        if (typeof options.delete === 'string') {
-            // Delete specific entry
-            ZSHRC.deleteFromZshrc(options.delete);
-        }
-        else {
-            // Interactive deletion
-            const entries = ZSHRC.listEntries();
-            const nameToDelete = await promptForEntryToDelete(entries);
-            if (nameToDelete) {
-                ZSHRC.deleteFromZshrc(nameToDelete);
-            }
-        }
+        return deletePath(options);
     }
     else if (options.lambda !== undefined) {
-        let func;
-        if (typeof options.lambda === 'string') {
-            // Function was provided as an argument
-            func = options.lambda;
-        }
-        else if (process.stdin.isTTY) {
-            // Interactive mode
-            func = await promptForFunction();
-        }
-        else {
-            // Non-interactive mode but no function provided
-            console.error('Error: Function argument required when piping output');
-            process.exit(1);
-        }
-        if (!func) {
-            console.log('No function provided. Exiting...');
-            process.exit(0);
-        }
-        // Get all arguments after the -l flag
-        const lambdaArgs = process.argv.slice(process.argv.indexOf('-l') + 1);
-        const command = generateShellCommand(func, lambdaArgs);
-        process.stdout.write(command + '\n');
-        // Prompt for an alias
-        const alias = await promptForAlias();
-        if (alias) {
-            ZSHRC.updateZshrc(alias, command);
-        }
+        return lambdaPath(options);
     }
 });
 program.parse();
